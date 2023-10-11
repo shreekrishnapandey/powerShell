5466455e443a429de4fdd8851726ea2ab767a247
Shree krishna pandey <shrikrishna.pandey1@gmail.com>
2023-10-11 09:05:45 +0545

    Update myscript.ps1

--- myscript.ps1
+++ myscript.ps1
@@ -1,4 +1,13 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-# This is a sample PowerShell script
-Write-Host "Hello, GitHub!"
+# Define your API key and city
+          $apiKey = '94cb7f7298ab35b30f2734eae3ca6e73'
+          $city = 'New York'  # Replace with the desired city
+
+          # Make the API request
+          $url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey"
+          $response = Invoke-RestMethod -Uri $url -Method Get
+
+          # Display the JSON response
+          Write-Host "Weather Response:"
+          $response | ConvertTo-Json -Depth
